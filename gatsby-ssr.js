9cbc23985654731157e772cce0ff60bf3f6f41b5
bilalhadid <hadid0648@gmail.com>
2021-01-15 22:09:42 +0500

    ALLL

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,8 @@
-import "firebase/auth";
+import React from "react";
+import Layout from "./src/component/Layout";
+
+export const wrapRootElement = ({ element, props }, pluginOptions) => (
+  <Layout {...props} features={pluginOptions.features}>
+    {element}
+  </Layout>
+);
